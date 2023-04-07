e1dd74973e9b5f226b2315d32e7f8aee7fc85a80
sevvalsaahinn <130187442+sevvalsaahinn@users.noreply.github.com>
2023-04-08 01:21:14 +0300

    Delete src directory

--- src/ElitUye.java
+++
@@ -1,6 +0,0 @@
-public class ElitUye extends Uye {
-    
-    public ElitUye(String isim, String soyisim, String email) {
-        super(isim,soyisim, email);
-    }
-}
--- src/GenelUye.java
+++
@@ -1,9 +0,0 @@
-
-
-public class GenelUye extends Uye {
-    
-    public GenelUye(String isim, String soyisim, String email) {
-        super(isim,soyisim, email);
-    }
-
-}
